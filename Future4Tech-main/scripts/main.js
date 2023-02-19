3dd11c2dbbfc1226aea2f2bc7729878c3abe628a
Edson-Cesari <107436497+Edson-Cesari@users.noreply.github.com>
2023-02-19 16:18:41 -0300

    Adicao ação do carrossel, correcao menu lateral

--- Future4Tech-main/scripts/main.js
+++ Future4Tech-main/scripts/main.js
@@ -1,5 +1,10 @@
 let count = 1;
 document.getElementById("radio1").checked = true;
+let btnSaibaMais = document.getElementById("btnSaibaMais");
+
+function redirecionar(caminho){
+    window.location.href=caminho;
+}
 
 setInterval(function(){
     passaImagem();
@@ -14,3 +19,26 @@ function passaImagem(){
     document.getElementById("radio"+count).checked = true;
 
 }
+
+btnSaibaMais.addEventListener('click',()=>{
+    
+    if(document.getElementById("radio1").isChecked){
+        redirecionar("./html/solucoes.html");
+
+    }else if(document.getElementById("radio2").checked){
+        redirecionar("./html/solucoes.html");
+
+    }else if(document.getElementById("radio3").checked){
+        redirecionar("./html/portifolio.html");
+
+    }else if(document.getElementById("radio4").checked){
+        redirecionar("./html/quemSomos.html");
+
+    }else if(document.getElementById("radio5").checked){
+        redirecionar("./html/faleConosco.html");
+
+    }else{
+        redirecionar("./index.html");
+
+    }
+});
